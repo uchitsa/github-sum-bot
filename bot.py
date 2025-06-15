0de04d1f5e58cd7b27c7071306af6b4c39f2fca8
uchitsa <uchitsa@ya.ru>
2025-06-16 02:47:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 
+import os
+import requests
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Application, CallbackContext, CommandHandler, MessageHandler, filters
 
+TG_TOKEN = os.getenv("TG_TOKEN")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+HEADERS = {"Authorization":f"{GITHUB_TOKEN}"}
 
 async def start(upd: Update, ctx: CallbackContext):
     await upd.message.reply_text("Send me a Github username")
+
 
 
 def get_github_data(username: str):
@@ -21,6 +27,21 @@ def get_github_data(username: str):
         }}
     }}
     """
+    resp = requests.post("https://api.github.com/graphql", json={"query":query}, headers=HEADERS)
+    data = resp.json().get("data", {}.get("user"))
+    if not data:
+        raise ValueError("USer not found or no data exist")
+    stars = data["starredRepositories"]["totalCount"]
+
+    return summary_result()
+
+
+def summary_result(username: str, name: str, stars: int):
+    return f"""
+    <b>{name}'s github report</b> (@{username})
+    Stars: <code>{stars}</code>
+    {datetime.now().strftime('%Y-%m-%d')}
+    """
 
 
 async def handle_username(upd: Update, ctx: CallbackContext):
@@ -29,7 +50,8 @@ async def handle_username(upd: Update, ctx: CallbackContext):
     await upd.message.reply_html(summary, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Share report")]]))
 
 
-app = Application.builder().token("TG_TOKEN").build()
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_username))
-app.run_polling()
+if __name__ == '__main__':
+    app = Application.builder().token("TG_TOKEN").build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_username))
+    app.run_polling()
