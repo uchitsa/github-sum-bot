2ee60508da852ebc251b775820936046f58a0769
uchitsa <uchitsa@ya.ru>
2025-07-02 16:58:36 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -2,11 +2,12 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import requests
+import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TELEGRAM_BOT_TOKEN = 'TELEGRAM_BOT_TOKEN'
+TELEGRAM_BOT_TOKEN = os.getenv("GITHUB_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Отправьте /github <username>, чтобы увидеть статистику профиля.")
